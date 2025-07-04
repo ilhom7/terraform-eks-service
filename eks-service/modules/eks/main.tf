3b0f6c047eea6af6e06cb6a36825f1705089e7a8
Ubuntu <ubuntu@ip-172-31-12-167.us-east-2.compute.internal>
2025-07-04 14:21:48 +0000

    modified main.tf

--- eks-service/modules/eks/main.tf
+++ eks-service/modules/eks/main.tf
@@ -106,15 +106,3 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 data "aws_availability_zones" "available" {}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
-}
-
-output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "cluster_token" {
-  value = data.aws_eks_cluster_auth.this.token
-}
